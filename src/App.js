6a941c3b8617efc1c7ca40d0fe1a4766145327b2
faranora21 <farahuorm21@gmail.com>
2023-06-02 05:34:43 +0800

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
        <div className="container">
-      <h1>Weather App</h1>
+      <Weather />
       <footer>
         This project was coded by Farah and is 
         <a href="https://github.com/faranora21/react-weather-app" target="_blank" rel="noreferrer" > open-sourced on GitHub</a>
---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Weather.css";
+
+
+export default function Weather() {
+    return (
+        <div className="Weather">
+            <form>
+            <div className="row">
+              <div className="col-9">      
+            <input type="search" placeholder="Enter a city" className="form-control" autoFocus="on"/> 
+            </div> 
+            <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary w-100" />
+            </div>
+            </div>
+            </form>
+            <h1>Johor Bahru</h1>
+            <ul>
+            <li>Wednesday, 02:30</li>
+            <li>Mostly Cloudy</li>
+            </ul>
+
+            <div className="row">
+                <div className="col-6">
+                    <img 
+                    src="//ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+                    alt="Clear with periodic cloud"/>
+                   <span className="temperature">6</span> 
+                   <span className="unit">°C</span>
+                </div>
+                <div className="col-6">
+                    <ul>
+                        <li>Precipitation: 3%</li>
+                        <li>Humidity: 89%</li>
+                        <li>Wind: 0 km/h</li>
+                    </ul>
+                </div>
+            </div>  
+
+            </div>
+         
+    )
+}
