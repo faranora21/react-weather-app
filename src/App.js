c772aa19d0aec965c3cfbd0882df4fa323298a84
faranora21 <farahuorm21@gmail.com>
2023-06-02 02:13:55 +0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+       <div className="container">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Farah and is 
+        <a href="https://github.com/faranora21/react-weather-app" target="_blank" rel="noreferrer" > open-sourced on GitHub</a>
+      </footer>
+      </div>
     </div>
   );
 }
